804f75ff6d9bef9d54d128f6d45e1138252986e8
Justyn Hunter <cjhunter@me.com>
2024-07-26 13:23:23 -0500

    adds treesj

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,85 +1,96 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects"
-    },
-    config = function()
-        require 'nvim-treesitter.configs'.setup {
-            "nvim-treesitter/playground",
-            modules = {},
-            ignore_install = {},
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = {
-                "astro",
-                "bash",
-                "c_sharp",
-                "css",
-                "dockerfile",
-                "eex",
-                "elixir",
-                "gitignore",
-                "gleam",
-                "go",
-                "graphql",
-                "heex",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "markdown_inline",
-                "ocaml",
-                "ocaml_interface",
-                "prisma",
-                "query",
-                "regex",
-                "templ",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-            },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-            highlight = {
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-
-            -- enable indentation
-            indent = { enable = false },
-
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects"
+        },
+        config = function()
+            require 'nvim-treesitter.configs'.setup {
+                "nvim-treesitter/playground",
+                modules = {},
+                ignore_install = {},
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                ensure_installed = {
+                    "astro",
+                    "bash",
+                    "c_sharp",
+                    "css",
+                    "dockerfile",
+                    "eex",
+                    "elixir",
+                    "gitignore",
+                    "gleam",
+                    "go",
+                    "graphql",
+                    "heex",
+                    "html",
+                    "javascript",
+                    "json",
+                    "lua",
+                    "markdown",
+                    "markdown_inline",
+                    "ocaml",
+                    "ocaml_interface",
+                    "prisma",
+                    "query",
+                    "regex",
+                    "templ",
+                    "tsx",
+                    "typescript",
+                    "vim",
+                    "vimdoc",
+                    "yaml",
                 },
-            },
-        }
 
-        vim.filetype.add({
-            extension = {
-                templ = "templ",
-            },
-        })
-    end
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
+
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+                highlight = {
+                    enable = true,
+
+                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                    -- Instead of true it can also be a list of languages
+                    additional_vim_regex_highlighting = false,
+                },
+
+                -- enable indentation
+                indent = { enable = false },
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        scope_incremental = false,
+                        node_decremental = "<bs>",
+                    },
+                },
+            }
+
+            vim.filetype.add({
+                extension = {
+                    templ = "templ",
+                },
+            })
+        end
+    },
+    {
+        "Wansmer/treesj",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            local treesj = require("treesj")
+            treesj.setup({})
+            vim.keymap.set("n", "<leader>j", ":TSJToggle<CR>")
+        end
+    }
 }
