aa24a9ed977f5a623783f0c9575ff9a6c66d7860
Justyn Hunter <jhunter@gsandf.com>
2024-08-12 09:44:19 -0500

    adds keybind to toggle git blame

--- .config/nvim/lua/plugins/gitblame.lua
+++ .config/nvim/lua/plugins/gitblame.lua
@@ -2,5 +2,7 @@ return {
     "FabijanZulj/blame.nvim",
     config = function()
         require("blame").setup()
+
+        vim.keymap.set("n", "<leader>gb", ":BlameToggle<CR>", { desc = "Toggle Git Blame" })
     end
 }
