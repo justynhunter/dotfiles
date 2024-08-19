5e88002cf31c0baeb5d7d117264a77fc443edf26
Justyn Hunter <jhunter@gsandf.com>
2024-08-19 12:45:03 -0500

    get statusline stuff from noice and put it here too

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,6 +11,11 @@ return {
                     {
                         "filename", path = 1
                     },
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                        color = { fg = "#ff9e64" },
+                    },
                 },
             },
         })
