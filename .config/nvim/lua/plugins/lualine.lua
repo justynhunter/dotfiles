2fa614db72435624ba98abb0c957a340795ce663
Justyn Hunter <jhunter@gsandf.com>
2024-08-19 12:44:37 -0500

    show full path in status line

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,7 +5,14 @@ return {
         theme = "base16"
     },
     config = function()
-        require("lualine").setup({})
-        -- local theme = require("lualine.themes.seoul256")
+        require("lualine").setup({
+            sections = {
+                lualine_c = {
+                    {
+                        "filename", path = 1
+                    },
+                },
+            },
+        })
     end
 }
