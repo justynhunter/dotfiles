bd65bb8a1293489178fff621d99c9d107651a815
Justyn Hunter <jhunter@gsandf.com>
2024-08-26 21:08:29 -0500

    chagne to catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
-    "savq/melange-nvim",
-    -- "catppuccin/nvim",
+    -- "savq/melange-nvim",
+    "catppuccin/nvim",
     -- "rose-pine/neovim",
     lazy = false,
-    name = "melange",
-    -- name = "catppuccin",
+    -- name = "melange",
+    name = "catppuccin",
     -- name = "rose-pine",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme "melange"
+        -- vim.cmd.colorscheme "melange"
 
-        -- require("catppuccin").setup({
-        --     flavour = "frappe"
-        -- })
-        -- vim.cmd.colorscheme "catppuccin"
+        require("catppuccin").setup({
+            flavour = "frappe"
+        })
+        vim.cmd.colorscheme "catppuccin"
 
         -- vim.cmd.colorscheme "rose-pine"
     end
