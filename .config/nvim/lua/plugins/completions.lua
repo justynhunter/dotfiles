7b65705b433225dcdf4a18fb77112df9bf7d915e
Justyn Hunter <jhunter@gsandf.com>
2024-08-26 21:08:22 -0500

    cleanup

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -23,7 +23,7 @@ return {
                 },
                 window = {
                     completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered()
+                    documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -35,7 +35,6 @@ return {
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
                     { name = "luasnip" },
-                }, {
                     { name = "buffer" }
                 }),
             }
