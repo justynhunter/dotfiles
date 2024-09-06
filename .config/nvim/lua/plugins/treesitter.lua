ecd5533ebd889244e134c177aacfb68ca8ef0a33
Justyn Hunter <jhunter@gsandf.com>
2024-09-06 15:22:53 -0500

    change keybind

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -69,8 +69,8 @@ return {
                 incremental_selection = {
                     enable = true,
                     keymaps = {
-                        init_selection = "<C-space>",
-                        node_incremental = "<C-space>",
+                        init_selection = "<M-space>",
+                        node_incremental = "<M-space>",
                         scope_incremental = false,
                         node_decremental = "<bs>",
                     },
