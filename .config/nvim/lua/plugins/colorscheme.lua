133ae8a24a31603687a4bede25056c0064c0f0c7
Justyn Hunter <cjhunter@me.com>
2024-09-20 22:36:03 -0500

    change everything to light mode

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     config = function()
         require("catppuccin").setup({
-            flavour = "frappe"
+            flavour = "latte"
         })
         vim.cmd.colorscheme "catppuccin"
     end,
