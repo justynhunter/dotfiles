14f93be161ca8d8c2041b7a85022a60de17696d6
Justyn Hunter <jhunter@gsandf.com>
2024-10-01 09:17:02 -0500

    go back to light theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
--- return {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         require("catppuccin").setup({
---             -- flavour = "latte"
---             flavour = "frappe"
---         })
---         vim.cmd.colorscheme "catppuccin"
---     end,
--- }
-
 return {
-    "savq/melange-nvim",
-    name = "melange",
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
     config = function()
-        vim.cmd.colorscheme "melange"
-    end
+        require("catppuccin").setup({
+            flavour = "latte"
+            -- flavour = "frappe"
+        })
+        vim.cmd.colorscheme "catppuccin"
+    end,
 }
+
+-- return {
+--     "savq/melange-nvim",
+--     name = "melange",
+--     config = function()
+--         vim.cmd.colorscheme "melange"
+--     end
+-- }
