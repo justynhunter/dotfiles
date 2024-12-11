f46198dc7764dd8bedb7eb91093107c6dad5d012
Justyn Hunter <jhunter@gsandf.com>
2024-12-11 11:01:24 -0600

    change fold expr

--- .config/nvim/lua/justynhunter/set.lua
+++ .config/nvim/lua/justynhunter/set.lua
@@ -41,7 +41,7 @@ vim.g.dotnet_show_project_file = false
 
 -- folds
 vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldcolumn = "0"
 vim.opt.foldtext = ""
 vim.opt.foldlevel = 99
