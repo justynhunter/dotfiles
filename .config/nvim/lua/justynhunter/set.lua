52bc691c3daae312b2ce337ac081f93cafb84749
Justyn Hunter <cjhunter@me.com>
2024-12-14 14:35:52 -0600

    gleam tabs

--- .config/nvim/lua/justynhunter/set.lua
+++ .config/nvim/lua/justynhunter/set.lua
@@ -4,8 +4,9 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
 
+vim.opt.expandtab = true
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = true
