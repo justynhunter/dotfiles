f9eb4483cff3bb713ecc3994109a766325d8607a
Justyn Hunter <jhunter@gsandf.com>
2025-01-29 21:39:39 -0600

    formatting

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,14 +1,23 @@
 return {
     "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("conform").setup({
+            formatters = {
+                dprint = {
+                    condition = function(ctx)
+                        ---@diagnostic disable-next-line: return-type-mismatch, undefined-field
+                        return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+                    end
+                }
+            },
             formatters_by_ft = {
                 astro = { "dprint" },
-                javascript = { "dprint", "biome" },
-                json = { "dprint", "biome" },
+                javascript = { "dprint" },
+                json = { "dprint" },
                 lua = { "stylua" },
                 markdown = { "deno fmt" },
-                typescript = { "dprint", "biome" },
+                typescript = { "dprint" },
             },
             format_on_save = {
                 -- These options will be passed to conform.format()
