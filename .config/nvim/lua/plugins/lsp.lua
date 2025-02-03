1d92d550141cd6324cc6533428726dadc694431f
Justyn Hunter <cjhunter@me.com>
2025-02-02 22:28:45 -0600

    lsp mappings

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -67,13 +67,13 @@ return {
                         return { buffer = true, desc = desc }
                     end
 
-                    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufmap("rename symbol"))
+                    vim.keymap.set("n", "grn", vim.lsp.buf.rename, bufmap("rename symbol"))
                     vim.keymap.set("n", "<leader>ll", vim.lsp.codelens.run, bufmap("run codelens"))
                     vim.keymap.set("n", "<leader>lL", vim.lsp.codelens.refresh, bufmap("refresh and run codelens"))
 
-                    vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, bufmap("signature help"))
-                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufmap("go to declaration"))
-                    vim.keymap.set("n", "ga", vim.lsp.buf.code_action, bufmap("code actions"))
+                    vim.keymap.set("n", "grs", vim.lsp.buf.signature_help, bufmap("signature help"))
+                    vim.keymap.set("n", "grD", vim.lsp.buf.declaration, bufmap("go to declaration"))
+                    vim.keymap.set("n", "gra", vim.lsp.buf.code_action, bufmap("code actions"))
                     vim.keymap.set("n", "gl", vim.diagnostic.open_float, bufmap("show diagnostic in float"))
                     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, bufmap("next diagnostic"))
                     vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, bufmap("previous diagnostic"))
@@ -83,4 +83,10 @@ return {
             vim.opt.completeopt = { "menu", "menuone", "noselect" }
         end
     },
+    {
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        config = function()
+            require("lsp_lines").setup()
+        end
+    }
 }
