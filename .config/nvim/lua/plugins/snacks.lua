aac6d3152c285a2e92a5eae2c759aea33f33dc83
Justyn Hunter <cjhunter@me.com>
2025-02-24 22:57:27 -0600

    picker config

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,3 +1,7 @@
+local function normal_mode()
+    vim.cmd.stopinsert()
+end
+
 return {
     "folke/snacks.nvim",
     priority = 1000,
@@ -7,20 +11,24 @@ return {
         animate = { enabled = true },
         bigfile = { enabled = true },
         picker = { enabled = true },
-        quickfile = { enabled = true },
+        quickfile = {
+            enabled = true,
+            matcher = { frecency = true },
+        },
     },
     keys = {
         -- picker
-        { "<leader><space>", function() Snacks.picker.smart() end,                desc = "Smart Find Files" },
-        { "<leader>fb",      function() Snacks.picker.buffers() end,              desc = "Find Buffers" },
-        { "<leader>ff",      function() Snacks.picker.grep() end,                 desc = "Find Grep" },
-        { "<leader>fc",      function() Snacks.picker.command_history() end,      desc = "Find Command History" },
-        { "<leader>fn",      function() Snacks.picker.notifications() end,        desc = "Find Notifications" },
-        { "<leader>fr",      function() Snacks.picker.recent() end,               desc = "Find Recent" },
-        { "<leader>fC",      function() Snacks.picker.commands() end,             desc = "Find Commands" },
-        { "grr",             function() Snacks.picker.lsp_references() end,       desc = "Find References" },
-        { "grd",             function() Snacks.picker.lsp_definitions() end,      desc = "Find Definitions" },
-        { "gri",             function() Snacks.picker.lsp_implementations() end,  desc = "Find Definitions" },
-        { "grt",             function() Snacks.picker.lsp_type_definitions() end, desc = "Find Types" },
+        { "<leader><space>", function() Snacks.picker.smart() end,                                         desc = "Smart Find Files" },
+        { "<leader>fb",      function() Snacks.picker.buffers({ on_show = normal_mode }) end,              desc = "Find Buffers" },
+        { "<leader>ff",      function() Snacks.picker.grep() end,                                          desc = "Find Grep" },
+        { "<leader>fc",      function() Snacks.picker.command_history() end,                               desc = "Find Command History" },
+        { "<leader>fk",      function() Snacks.picker.keymaps() end,                                       desc = "Find keymaps" },
+        { "<leader>fn",      function() Snacks.picker.notifications() end,                                 desc = "Find Notifications" },
+        { "<leader>fr",      function() Snacks.picker.recent() end,                                        desc = "Find Recent" },
+        { "<leader>fC",      function() Snacks.picker.commands() end,                                      desc = "Find Commands" },
+        { "grr",             function() Snacks.picker.lsp_references({ on_show = normal_mode }) end,       desc = "Find References" },
+        { "grd",             function() Snacks.picker.lsp_definitions({ on_show = normal_mode }) end,      desc = "Find Definitions" },
+        { "gri",             function() Snacks.picker.lsp_implementations({ on_show = normal_mode }) end,  desc = "Find Definitions" },
+        { "grt",             function() Snacks.picker.lsp_type_definitions({ on_show = normal_mode }) end, desc = "Find Types" },
     }
 }
