19efd3b9e71aab05b7add78a9722df33407ebe43
Justyn Hunter <cjhunter@me.com>
2025-02-25 22:56:00 -0600

    keymaps

--- .config/nvim/lua/justynhunter/remap.lua
+++ .config/nvim/lua/justynhunter/remap.lua
@@ -37,6 +37,10 @@ vim.keymap.set("n", "*", ":keepjumps normal! mi*`i<CR>")
 -- remove whitespace below
 vim.keymap.set("n", "J", "mzJ`z", { desc = "keep cursor location on join" })
 
+-- add whitespace
+vim.keymap.set("n", "\\", "mzO<Esc>`z", { desc = "adds line above" })
+vim.keymap.set("n", "|", "mzo<Esc>`z", { desc = "adds line below" })
+
 -- page down and up
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "down half page" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "up half page" })
@@ -57,10 +61,6 @@ vim.keymap.set("n", "<leader>sh", ":split<Enter>", { desc = "split horizontal" }
 vim.keymap.set("n", "<leader>sv", ":vsplit<Enter>", { desc = "split vertical" })
 vim.keymap.set("n", "<leader>sr", "<C-w>r", { desc = "rotate up/left" })
 vim.keymap.set("n", "<leader>sr", "<C-w>R", { desc = "rotate down/right" })
--- vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "move to left pane" })
--- vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "move to right pane" })
--- vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "move to up pane" })
--- vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "move to down pane" })
 vim.keymap.set("n", "<leader>c", "<cmd>bp<bar>bd #<CR>", { desc = "move to down pane" })
 vim.keymap.set("n", "<leader>C", "<cmd>bd<CR>", { desc = "move to down pane" })
 
@@ -68,7 +68,6 @@ vim.keymap.set("n", "<leader>C", "<cmd>bd<CR>", { desc = "move to down pane" })
 vim.keymap.set("n", "Q", "@q")
 
 -- buffers
-vim.keymap.set("n", "bb", "<cmd>b#<CR>", { desc = "switch to last open buffer" })
 vim.keymap.set("n", "H", "<cmd>bp<CR>", { desc = "previous buffer" })
 vim.keymap.set("n", "L", "<cmd>bn<CR>", { desc = "next buffer" })
 
