3029805543568786bc1ed8dc4ac0876bb537aa83
Justyn Hunter <cjhunter@me.com>
2025-03-13 00:13:00 -0500

    change to catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,55 +1,55 @@
-return {
-    "savq/melange-nvim",
-    name = "melange",
-    config = function()
-        vim.cmd.colorscheme "melange"
-    end
-}
-
 -- return {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     lazy = false,
---     priority = 1000,
+--     "savq/melange-nvim",
+--     name = "melange",
 --     config = function()
---         require("catppuccin").setup({
---             -- flavour = "latte"
---             flavour = "frappe",
---             integrations = {
---                 cmp = true,
---                 flash = true,
---                 fzf = true,
---                 mini = {
---                     enabled = true,
---                     indentscope_color = "",
---                 },
---                 native_lsp = {
---                     enabled = true,
---                     virtual_text = {
---                         errors = { "italic" },
---                         hints = { "italic" },
---                         warnings = { "italic" },
---                         information = { "italic" },
---                         ok = { "italic" },
---                     },
---                     underlines = {
---                         errors = { "underline" },
---                         hints = { "underline" },
---                         warnings = { "underline" },
---                         information = { "underline" },
---                         ok = { "underline" },
---                     },
---                     inlay_hints = {
---                         background = true,
---                     },
---                 },
---             },
---             custom_highlights = function(colors)
---                 return {
---                     MiniJump = { fg = colors.base, bg = colors.red },
---                 }
---             end,
---         })
---         vim.cmd.colorscheme "catppuccin"
---     end,
+--         vim.cmd.colorscheme "melange"
+--     end
 -- }
+
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            -- flavour = "latte"
+            flavour = "frappe",
+            integrations = {
+                cmp = true,
+                flash = true,
+                fzf = true,
+                mini = {
+                    enabled = true,
+                    indentscope_color = "",
+                },
+                native_lsp = {
+                    enabled = true,
+                    virtual_text = {
+                        errors = { "italic" },
+                        hints = { "italic" },
+                        warnings = { "italic" },
+                        information = { "italic" },
+                        ok = { "italic" },
+                    },
+                    underlines = {
+                        errors = { "underline" },
+                        hints = { "underline" },
+                        warnings = { "underline" },
+                        information = { "underline" },
+                        ok = { "underline" },
+                    },
+                    inlay_hints = {
+                        background = true,
+                    },
+                },
+            },
+            custom_highlights = function(colors)
+                return {
+                    MiniJump = { fg = colors.base, bg = colors.red },
+                }
+            end,
+        })
+        vim.cmd.colorscheme "catppuccin"
+    end,
+}
