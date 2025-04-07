291a32ac7866891bcb5c8cbbd6321a8024e22631
Justyn Hunter <cjhunter@me.com>
2025-04-06 23:55:56 -0500

    try out blink

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,24 @@
+return {
+    "saghen/blink.cmp",
+    dependencies = {
+        "rafamadriz/friendly-snippets",
+        { "L3MON4D3/LuaSnip", version = "2.*" },
+    },
+    version = "1.*",
+
+    --@module "blink.cmp"
+    --@type blink.cmp.Config
+    opts = {
+        keymap = { preset = "default" },
+        appearance = {
+            nerd_font_variant = "mono",
+        },
+        completion = { documentation = { auto_show = true } },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+        },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+        snippets = { preset = "luasnip" },
+    },
+    opts_extend = { "sources.default" },
+}
