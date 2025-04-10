d1d07e5a18e5bbe619c229aeab42d545d7394c0d
Justyn Hunter <cjhunter@me.com>
2025-04-09 21:49:21 -0500

    emojie completions 🚀

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -2,6 +2,7 @@ return {
     "saghen/blink.cmp",
     dependencies = {
         "rafamadriz/friendly-snippets",
+        "moyiz/blink-emoji.nvim",
         { "L3MON4D3/LuaSnip", version = "2.*" },
     },
     version = "1.*",
@@ -16,7 +17,18 @@ return {
         completion = { documentation = { auto_show = true } },
         signature = { enabled = true },
         sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
+            default = { "lsp", "path", "snippets", "buffer", "emoji" },
+            providers = {
+                emoji = {
+                    module = "blink-emoji",
+                    name = "Emoji",
+                    score_offset = 15,
+                    opts = { insert = true },
+                    should_show_items = function()
+                        return vim.tbl_contains({ "gitcommit", "markdown" }, vim.o.filetype)
+                    end,
+                },
+            },
         },
         fuzzy = { implementation = "prefer_rust_with_warning" },
         snippets = { preset = "luasnip" },
