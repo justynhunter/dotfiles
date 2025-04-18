c2536c9d8b206dfc15bf6dc57cdeb02c4b7a4613
Justyn Hunter <jhunter@gsandf.com>
2025-04-18 09:30:50 -0500

    wez config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,33 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.color_scheme = 's3r0 modified (terminal.sexy)'
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+local mux = wezterm.mux
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+end)
+
+config.mouse_bindings = {
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+}
+
+config.term = "xterm-256color"
+
+config.native_macos_fullscreen_mode = true
+config.color_scheme = 'catppuccin-frappe'
 
 config.font = wezterm.font("JetBrains Mono", { weight = 600 })
 config.font_size = 12
 
 config.enable_tab_bar = false
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 
 return config
