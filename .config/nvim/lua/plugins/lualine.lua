1069774886633c996b691a350cd42460be86422b
Justyn Hunter <cjhunter@me.com>
2025-05-24 23:08:12 -0500

    show macro msg in lualine x

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -7,6 +7,12 @@ return {
     config = function()
         require("lualine").setup({
             sections = {
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                    },
+                },
                 lualine_c = {
                     {
                         "filename", path = 1
