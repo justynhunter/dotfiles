0751e204d0e023929b7c1b54333b95062226c4b0
Justyn Hunter <jhunter@gsandf.com>
2025-09-26 11:33:57 -0500

    pairs

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
     version = '*',
     config = function()
         require("mini.ai").setup()
+        require("mini.pairs").setup()
         require("mini.splitjoin").setup()
         require("mini.surround").setup()
     end
