b98a2ce72324ce68dec10eb3fa3dbdbf2fef6484
Justyn Hunter <cjhunter@me.com>
2025-11-15 22:03:41 -0600

    telescope

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,42 +1,140 @@
 local function normal_mode()
-    vim.cmd.stopinsert()
+	vim.cmd.stopinsert()
 end
 
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    --@type snacks.Config
-    opts = {
-        animate = { enabled = true },
-        bigfile = { enabled = true },
-        image = { enabled = false },
-        indent = { enabled = true, },
-        input = { enabled = true },
-        notifier = { enabled = true },
-        picker = { enabled = true },
-        quickfile = {
-            enabled = true,
-            matcher = { frecency = true },
-        },
-    },
-    keys = {
-        -- picker
-        { "<leader><space>", function() Snacks.picker.smart() end,                                        desc = "Smart Find Files" },
-        { "<leader>fb",      function() Snacks.picker.buffers({ on_show = normal_mode }) end,             desc = "Find Buffers" },
-        { "<M-b>",           function() Snacks.picker.buffers({ on_show = normal_mode }) end,             desc = "Find Buffers" },
-        { "<leader>ff",      function() Snacks.picker.grep() end,                                         desc = "Find Grep" },
-        { "<leader>f:",      function() Snacks.picker.command_history() end,                              desc = "Find Command History" },
-        { "<leader>fk",      function() Snacks.picker.keymaps() end,                                      desc = "Find keymaps" },
-        { "<leader>fn",      function() Snacks.picker.notifications() end,                                desc = "Find Notifications" },
-        { "<leader>fr",      function() Snacks.picker.recent() end,                                       desc = "Find Recent" },
-        { "<leader>fC",      function() Snacks.picker.commands() end,                                     desc = "Find Commands" },
-        { "<leader>fm",      function() Snacks.picker.marks({ on_show = normal_mode }) end,               desc = "Find Marks" },
-        { "<leader>fw",      function() Snacks.picker.grep_word() end,                                    desc = "Find Visual Selection", mode = { "n", "x" } },
-        { "gr",              function() Snacks.picker.lsp_references({ on_show = normal_mode }) end,      desc = "Find References" },
-        { "gd",              function() Snacks.picker.lsp_definitions({ on_show = normal_mode }) end,     desc = "Find Definitions" },
-        { "gD",              function() Snacks.picker.lsp_declarations({ on_show = normal_mode }) end,    desc = "Find Declarations" },
-        { "gi",              function() Snacks.picker.lsp_implementations({ on_show = normal_mode }) end, desc = "Find Implementations" },
-        { "gy",              function() Snacks.picker.lsp_type_definitions() end,                         desc = "Goto T[y]pe Definition" },
-    }
+	"folke/snacks.nvim",
+	enabled = false,
+	priority = 1000,
+	lazy = false,
+	--@type snacks.Config
+	opts = {
+		animate = { enabled = true },
+		bigfile = { enabled = true },
+		image = { enabled = false },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		picker = { enabled = true },
+		quickfile = {
+			enabled = true,
+			matcher = { frecency = true },
+		},
+	},
+	keys = {
+		-- picker
+		{
+			"<leader><space>",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers({ on_show = normal_mode })
+			end,
+			desc = "Find Buffers",
+		},
+		{
+			"<M-b>",
+			function()
+				Snacks.picker.buffers({ on_show = normal_mode })
+			end,
+			desc = "Find Buffers",
+		},
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Find Grep",
+		},
+		{
+			"<leader>f:",
+			function()
+				Snacks.picker.command_history()
+			end,
+			desc = "Find Command History",
+		},
+		{
+			"<leader>fk",
+			function()
+				Snacks.picker.keymaps()
+			end,
+			desc = "Find keymaps",
+		},
+		{
+			"<leader>fn",
+			function()
+				Snacks.picker.notifications()
+			end,
+			desc = "Find Notifications",
+		},
+		{
+			"<leader>fr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Find Recent",
+		},
+		{
+			"<leader>fC",
+			function()
+				Snacks.picker.commands()
+			end,
+			desc = "Find Commands",
+		},
+		{
+			"<leader>fm",
+			function()
+				Snacks.picker.marks({ on_show = normal_mode })
+			end,
+			desc = "Find Marks",
+		},
+		{
+			"<leader>fw",
+			function()
+				Snacks.picker.grep_word()
+			end,
+			desc = "Find Visual Selection",
+			mode = { "n", "x" },
+		},
+		{
+			"gr",
+			function()
+				Snacks.picker.lsp_references({ on_show = normal_mode })
+			end,
+			desc = "Find References",
+		},
+		{
+			"gd",
+			function()
+				Snacks.picker.lsp_definitions({ on_show = normal_mode })
+			end,
+			desc = "Find Definitions",
+		},
+		{
+			"gD",
+			function()
+				Snacks.picker.lsp_declarations({ on_show = normal_mode })
+			end,
+			desc = "Find Declarations",
+		},
+		{
+			"gi",
+			function()
+				Snacks.picker.lsp_implementations({ on_show = normal_mode })
+			end,
+			desc = "Find Implementations",
+		},
+		{
+			"gy",
+			function()
+				Snacks.picker.lsp_type_definitions()
+			end,
+			desc = "Goto T[y]pe Definition",
+		},
+	},
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,65 +1,66 @@
 return {
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        enabled = false
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        enabled = false,
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-            "nvim-lua/popup.nvim",
-            "nvim-telescope/telescope-media-files.nvim"
-        },
-        config = function()
-            local actions = require("telescope.actions")
-            require("telescope").setup({
-                extensions = {
-                    fzf = {},
-                    media_files = {
-                        filetypes = { "png", "jpg", "mp4", "webm", "pdf" },
-                        find_cmd = "rg"
-                    }
-                },
-                defaults = {
-                    mappings = {
-                        i = {
-                            ["<C-j>"] = actions.cycle_history_next,
-                            ["<C-k>"] = actions.cycle_history_prev,
-                        }
-                    }
-                }
-            })
-            require("telescope").load_extension("fzf")
-            require("telescope").load_extension("ui-select")
-            require("telescope").load_extension("media_files")
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		enabled = true,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		enabled = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-lua/popup.nvim",
+			"nvim-telescope/telescope-media-files.nvim",
+		},
+		config = function()
+			local actions = require("telescope.actions")
+			require("telescope").setup({
+				extensions = {
+					fzf = {},
+					media_files = {
+						filetypes = { "png", "jpg", "mp4", "webm", "pdf" },
+						find_cmd = "rg",
+					},
+				},
+				defaults = {
+					mappings = {
+						i = {
+							["<C-j>"] = actions.cycle_history_next,
+							["<C-k>"] = actions.cycle_history_prev,
+						},
+					},
+				},
+			})
+			require("telescope").load_extension("fzf")
+			require("telescope").load_extension("ui-select")
+			require("telescope").load_extension("media_files")
 
-            local builtin = require("telescope.builtin")
-            local ext = require("telescope").extensions
-            vim.keymap.set("n", "<leader> ", builtin.find_files, { desc = "find files" })
-            vim.keymap.set("v", "<leader> ", function()
-                local text = vim.getVisualSelection()
-                builtin.find_files({ default_text = text, desc = "find selection in files" })
-            end)
-            vim.keymap.set("n", "<leader>ff", builtin.live_grep, { desc = "Search by Grep" })
-            vim.keymap.set("v", "<leader>ff", function()
-                local text = vim.getVisualSelection()
-                builtin.live_grep({ default_text = text, desc = "Search selection by Grep" })
-            end)
-            vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "search keymaps" })
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "search buffers" })
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "search help" })
-            vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "search marks" })
-            vim.keymap.set("n", "<leader>fds", builtin.lsp_document_symbols, { desc = "search document symbols" })
-            vim.keymap.set("n", "<leader>fws", builtin.lsp_workspace_symbols, { desc = "search workspace symbols" })
-            vim.keymap.set("n", "<leader>fi", ext.media_files.media_files)
-            vim.keymap.set("n", "grr", builtin.lsp_references, { desc = "find references" })
-            vim.keymap.set("n", "grd", builtin.lsp_definitions, { desc = "goto definition" })
-            vim.keymap.set("n", "gri", builtin.lsp_implementations, { desc = "find implementations" })
-            vim.keymap.set("n", "gry", builtin.lsp_type_definitions, { desc = "find type" })
-            vim.keymap.set("n", "grci", builtin.lsp_incoming_calls, { desc = "find incoming calls" })
-            vim.keymap.set("n", "grco", builtin.lsp_outgoing_calls, { desc = "find outgoing calls" })
-        end
-    }
+			local builtin = require("telescope.builtin")
+			local ext = require("telescope").extensions
+			vim.keymap.set("n", "<leader> ", builtin.find_files, { desc = "find files" })
+			vim.keymap.set("v", "<leader> ", function()
+				local text = vim.getVisualSelection()
+				builtin.find_files({ default_text = text, desc = "find selection in files" })
+			end)
+			vim.keymap.set("n", "<leader>ff", builtin.live_grep, { desc = "Search by Grep" })
+			vim.keymap.set("n", "<leader>fw", function()
+				builtin.grep_string({ initial_mode = "normal" })
+			end, { desc = "Search by Grep" })
+			vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "search keymaps" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "search buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "search help" })
+			vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "search marks" })
+			vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "search registers" })
+			vim.keymap.set("n", "<leader>fs", builtin.spell_suggest, { desc = "spelling suggestions" })
+			vim.keymap.set("n", "<leader>fds", builtin.lsp_document_symbols, { desc = "search document symbols" })
+			vim.keymap.set("n", "<leader>fws", builtin.lsp_workspace_symbols, { desc = "search workspace symbols" })
+			vim.keymap.set("n", "<leader>fi", ext.media_files.media_files)
+			vim.keymap.set("n", "grr", builtin.lsp_references, { desc = "find references" })
+			vim.keymap.set("n", "grd", builtin.lsp_definitions, { desc = "goto definition" })
+			vim.keymap.set("n", "gri", builtin.lsp_implementations, { desc = "find implementations" })
+			vim.keymap.set("n", "gry", builtin.lsp_type_definitions, { desc = "find type" })
+			vim.keymap.set("n", "grci", builtin.lsp_incoming_calls, { desc = "find incoming calls" })
+			vim.keymap.set("n", "grco", builtin.lsp_outgoing_calls, { desc = "find outgoing calls" })
+		end,
+	},
 }
