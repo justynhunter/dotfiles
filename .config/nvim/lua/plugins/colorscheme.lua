bc776d318ae0ffcaaa12961fa946b08faea4fadf
Justyn Hunter <cjhunter@me.com>
2025-11-19 22:39:46 -0600

    frappe

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,49 +7,49 @@
 -- }
 
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("catppuccin").setup({
-            -- flavour = "latte"
-            -- flavour = "frappe",
-            integrations = {
-                cmp = true,
-                flash = true,
-                fzf = true,
-                mini = {
-                    enabled = true,
-                    indentscope_color = "",
-                },
-                native_lsp = {
-                    enabled = true,
-                    virtual_text = {
-                        errors = { "italic" },
-                        hints = { "italic" },
-                        warnings = { "italic" },
-                        information = { "italic" },
-                        ok = { "italic" },
-                    },
-                    underlines = {
-                        errors = { "underline" },
-                        hints = { "underline" },
-                        warnings = { "underline" },
-                        information = { "underline" },
-                        ok = { "underline" },
-                    },
-                    inlay_hints = {
-                        background = true,
-                    },
-                },
-            },
-            custom_highlights = function(colors)
-                return {
-                    MiniJump = { fg = colors.base, bg = colors.red },
-                }
-            end,
-        })
-        vim.cmd.colorscheme "catppuccin"
-    end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			-- flavour = "latte"
+			flavour = "frappe",
+			integrations = {
+				cmp = true,
+				flash = true,
+				fzf = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { "italic" },
+						hints = { "italic" },
+						warnings = { "italic" },
+						information = { "italic" },
+						ok = { "italic" },
+					},
+					underlines = {
+						errors = { "underline" },
+						hints = { "underline" },
+						warnings = { "underline" },
+						information = { "underline" },
+						ok = { "underline" },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+			},
+			custom_highlights = function(colors)
+				return {
+					MiniJump = { fg = colors.base, bg = colors.red },
+				}
+			end,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
